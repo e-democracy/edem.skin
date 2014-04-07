53b23f8646db448ce0e32b426f586c3b4f29f421
William Bushey <wbushey@gmail.com>
2014-04-07 13:38:06 -0500

    Created JS to add buttons for anonymous users on client side

---
+++ edem/skin/browser/js/edem-skin-add-anonymous-buttons.js
@@ -0,0 +1,14 @@
+"use strict";
+jQuery.noConflict();
+
+function edem_skin_add_anonymous_buttons(){
+    if (!jQuery('#edem-content-js-data').data('loggedin')){
+        var about_anon_buttons_id = '.edem-site-about-anonymous-buttons', about_anon_buttons = jQuery(about_anon_buttons_id);
+        about_anon_buttons.html([
+            '<a class="btn" href="/getinvolved/join" style="display:block; margin-bottom:.5em;">Find a Form</a>',
+            '<a class="btn" href="/login.html?came_from=/" data-icon="🔒" style="display:block; margin-bottom:.5em;">Sign In</a>'
+        ].join('\n'));
+    }
+}
+
+jQuery(window).load(edem_skin_add_anonymous_buttons);
