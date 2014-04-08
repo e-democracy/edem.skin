cfb5be29334fc1d2ef68db454f7d82884546703a
Bill Bushey <wbushey@gmail.com>
2014-04-08 10:22:00 -0500

    Fixed a typo

--- edem/skin/browser/js/edem-skin-add-anonymous-buttons.js
+++ edem/skin/browser/js/edem-skin-add-anonymous-buttons.js
@@ -5,7 +5,7 @@ function edem_skin_add_anonymous_buttons(){
     if (!jQuery('#edem-content-js-data').data('loggedin')){
         var about_anon_buttons_id = '.edem-site-about-anonymous-buttons', about_anon_buttons = jQuery(about_anon_buttons_id);
         about_anon_buttons.html([
-            '<a class="btn" href="/getinvolved/join" style="display:block; margin-bottom:.5em;">Find a Form</a>',
+            '<a class="btn" href="/getinvolved/join" style="display:block; margin-bottom:.5em;">Find a Forum</a>',
             '<a class="btn" href="/login.html?came_from=/" data-icon="🔒" style="display:block; margin-bottom:.5em;">Sign In</a>'
         ].join('\n'));
     }
