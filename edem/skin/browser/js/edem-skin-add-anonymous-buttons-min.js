abd72ca320d2c164d14f260c31ce887c2654c40c
Bill Bushey <wbushey@gmail.com>
2014-04-08 10:34:31 -0500

    Minifying the last JS changes

--- edem/skin/browser/js/edem-skin-add-anonymous-buttons-min.js
+++ edem/skin/browser/js/edem-skin-add-anonymous-buttons-min.js
@@ -1,3 +1,3 @@
 "use strict";jQuery.noConflict();function edem_skin_add_anonymous_buttons(){if(!jQuery("#edem-content-js-data").data("loggedin")){var b=".edem-site-about-anonymous-buttons",a=jQuery(b);
-a.html(['<a class="btn" href="/getinvolved/join" style="display:block; margin-bottom:.5em;">Find a Form</a>','<a class="btn" href="/login.html?came_from=/" data-icon="🔒" style="display:block; margin-bottom:.5em;">Sign In</a>'].join("\n"))
+a.html(['<a class="btn" href="/getinvolved/join" style="display:block; margin-bottom:.5em;">Find a Forum</a>','<a class="btn" href="/login.html?came_from=/" data-icon="🔒" style="display:block; margin-bottom:.5em;">Sign In</a>'].join("\n"))
 }}jQuery(window).load(edem_skin_add_anonymous_buttons);
