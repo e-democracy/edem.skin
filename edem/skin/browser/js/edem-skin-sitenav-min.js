6cb0fafd4230e80f2ec9066613a0aa1f65c86510
Bill Bushey <wbushey@gmail.com>
2014-07-23 19:51:34 -0500

    Moving the Account items of sitenav to the top of the menu on small screens

--- edem/skin/browser/js/edem-skin-sitenav-min.js
+++ edem/skin/browser/js/edem-skin-sitenav-min.js
@@ -1,5 +1,6 @@
-"use strict";function setSitenavBehavior(){if(jQuery(".navbar-header").css("float")=="none"){jQuery("#sitenav .dropdown-toggle").attr("data-toggle","")
-}else{jQuery("#sitenav .dropdown-toggle").attr("data-toggle","dropdown")}}jQuery(window).resize(setSitenavBehavior);
+"use strict";function setSitenavBehavior(){if(jQuery(".navbar-header").css("float")=="none"){jQuery("#sitenav .dropdown-toggle").attr("data-toggle","");
+jQuery("#sitenav-account").detach().prependTo("#sitenav")}else{jQuery("#sitenav .dropdown-toggle").attr("data-toggle","dropdown");
+jQuery("#sitenav-account").detach().appendTo("#sitenav")}}jQuery(window).resize(setSitenavBehavior);
 jQuery(window).load(setSitenavBehavior);jQuery("#sitenav-collapse").on("show",function(){jQuery("#sitenav .dropdown").addClass("open")
 });jQuery("#sitenav-collapse").on("hidden",function(){jQuery("#sitenav .dropdown").removeClass("open")
 });
--- edem/skin/browser/js/edem-skin-sitenav.js
+++ edem/skin/browser/js/edem-skin-sitenav.js
@@ -2,14 +2,17 @@
 /*
  * Disable the toggling of dropdown menus on small screens, enable on larger 
  * screens. 
+ * Also move the Account items of the menu to the top on small screens
  */
 function setSitenavBehavior(){
     // .navbar-header float is used to observe when the small screen media
     // query has been tripped
     if (jQuery(".navbar-header").css("float") == "none" ){
 		jQuery("#sitenav .dropdown-toggle").attr('data-toggle', '');
+        jQuery("#sitenav-account").detach().prependTo("#sitenav");
 	} else {
 		jQuery("#sitenav .dropdown-toggle").attr('data-toggle', 'dropdown');
+        jQuery("#sitenav-account").detach().appendTo("#sitenav");
 	}
 }
 
