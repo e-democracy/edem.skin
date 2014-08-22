36304d230b72d76057403094ffdec49021162536
William Bushey <wbushey@gmail.com>
2014-08-22 15:38:44 -0500

    Fixed the versions file

--- version.py
+++ version.py
@@ -1,25 +1,28 @@
-#coding=utf-8
+# -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
 #-----------------------------------------------------------------------------#
 
-version = '2.0'
+version = '2.0.0'
 release = False
 
 #-----------------------------------------------------------------------------#
 
-import commands
+import sys
+if (sys.version_info < (3, )):
+    from commands import getstatusoutput
+else:
+    from subprocess import getstatusoutput  # lint:ok
 import datetime
 import os
 import glob
-
 
 class CommandError(Exception):
     pass
 
 
 def execute_command(commandstring):
-    status, output = commands.getstatusoutput(commandstring)
+    status, output = getstatusoutput(commandstring)
     if status != 0:
         raise CommandError
     return output
@@ -33,7 +36,7 @@ def parse_version_from_package():
 
     version_string = ''
     if os.path.exists(pkginfo):
-        for line in file(pkginfo):
+        for line in open(pkginfo):
             if line.find('Version: ') == 0:
                 version_string = line.strip().split('Version: ')[1].strip()
         if not version_string:
@@ -51,12 +54,17 @@ def get_version():
         dt = datetime.datetime.utcfromtimestamp(float(commitdate))
         datestring = dt.strftime('%Y%m%d%H%M%S')
 
-        version_string = "%s-%s-%s" % (version, datestring, commithash)
+        if release:
+            version_string = version
+        else:
+            version_string = "%s.dev%s-%s" % (version, datestring, commithash)
 
     except CommandError:
         version_string = parse_version_from_package()
 
     return version_string
 
+
 if __name__ == '__main__':
-    print get_version()
+    import sys
+    sys.stdout.write('{0}\n'.format(get_version()))
